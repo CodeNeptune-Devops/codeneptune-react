4c011ddcc8ccc8aa28a7db84e189be0eeaa1beac
SANJAYD <sanjayjay564@gmail.com>
2025-11-18 09:55:46 +0530

    text clip fix check

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,13 +3,15 @@
 import React, { useEffect, useState, useRef } from "react";
 import Image from "next/image";
 import dynamic from "next/dynamic";
-import { Plus_Jakarta_Sans } from "next/font/google";
 import ContactModal from "@/modals/ContactModal";
 import { Bebas_Neue } from "next/font/google";
-import Link from "next/link";
 
-const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ["latin"], weight: "500" });
-const bebas_neue = Bebas_Neue({ subsets: ["latin"], weight: "400" });
+const bebas_neue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
+
 
 // ✅ Lazy-load all animated text components
 const BlurText = dynamic(() => import("@/animatedTexts/blurText/BlurText"), {
@@ -24,12 +26,12 @@ const GradientText = dynamic(
 // });
 
 export default function Hero({
-  text1="Crafting Websites, Mobile Apps &",
-  text2="AI Agents That Redefine Possibility",
-  description="As a leading website and mobile app development company, we craft intelligent, AI-driven digital products that transform ideas into scalable, future-ready growth.",
-  buttonText="Start Your Project"
+  text1 = "Crafting Websites, Mobile Apps &",
+  text2 = "AI Agents That Redefine Possibility",
+  description = "As a leading website and mobile app development company, we craft intelligent, AI-driven digital products that transform ideas into scalable, future-ready growth.",
+  buttonText = "Start Your Project"
 }) {
- const scrollRef = useRef(null);
+  const scrollRef = useRef(null);
   const [translateY, setTranslateY] = useState(0);
   const [headingFadeComplete, setHeadingFadeComplete] = useState(false);
   const [headingScaleComplete, setHeadingScaleComplete] = useState(false);
@@ -80,9 +82,8 @@ export default function Hero({
       <div className="relative z-20 max-w-7xl mx-auto w-full flex flex-col items-center gap-8 md:gap-4 px-2 py-12">
         {/* Badge */}
         <div
-          className={`transition-all duration-700 ease-out flex justify-center items-center w-full ${
-            isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
-          }`}
+          className={`transition-all duration-700 ease-out flex justify-center items-center w-full ${isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
+            }`}
           style={{ willChange: "transform, opacity" }}
         >
           <button className="flex flex-row justify-center items-center gap-1 sm:gap-2 px-3 sm:px-6 py-3 rounded-full border border-white/20 bg-white/5 backdrop-blur-sm hover:bg-white/10 transition-colors">
@@ -103,11 +104,10 @@ export default function Hero({
         <div className="flex flex-col items-center justify-center  gap-4 mt-0 min-h-[120px] sm:min-h-[200px]  w-[13rem] sm:w-full">
           <div className="flex flex-col w-full justify-center items-center gap-10 sm:gap-2 sm:gap-2">
             <h2
-              className={`hidden sm:block text-lg md:text-4xl lg:text-5xl text-center font-extrabold transition-all ease-out ${
-                !headingFadeComplete
+              className={`hidden sm:block text-lg md:text-4xl lg:text-5xl text-center font-extrabold transition-all ease-out ${!headingFadeComplete
                   ? "scale-150 opacity-0"
                   : "scale-150 opacity-100"
-              } ${headingPositionComplete ? "translate-y-0" : "translate-y-[10vh]"}`}
+                } ${headingPositionComplete ? "translate-y-0" : "translate-y-[10vh]"}`}
               style={{
                 transitionDuration: "800ms",
                 willChange: "transform, opacity",
@@ -119,17 +119,16 @@ export default function Hero({
                   delay={150}
                   animateBy="words"
                   direction="top"
-                  className={`text-lg md:text-4xl lg:text-5xl text-center justify-center leading-relaxed text-white font-extrabold ${plus_jakarta_sans.className}`}
+                  className={`text-lg md:text-4xl lg:text-5xl text-center justify-center leading-relaxed text-white font-extrabold ${bebas_neue.className}`}
                 />
               </div>
             </h2>
 
-             <h2
-              className={`sm:hidden block text-lg md:text-4xl lg:text-5xl text-center font-extrabold transition-all ease-out ${
-                !headingFadeComplete
+            <h2
+              className={`sm:hidden block text-lg md:text-4xl lg:text-5xl text-center font-extrabold transition-all ease-out ${!headingFadeComplete
                   ? "scale-150 opacity-0"
                   : "scale-150 opacity-100"
-              } ${headingPositionComplete ? "translate-y-0" : "translate-y-[10vh]"}`}
+                } ${headingPositionComplete ? "translate-y-0" : "translate-y-[10vh]"}`}
               style={{
                 transitionDuration: "800ms",
                 willChange: "transform, opacity",
@@ -147,9 +146,8 @@ export default function Hero({
             </h2>
 
             <h2
-              className={`hidden sm:block text-xl md:text-5xl lg:text-5xl text-center scale-150 leading-tight bg-gradient-to-r from-[#40ffaa] via-[#4079ff] to-[#40ffaa] bg-clip-text text-transparent transition-all duration-700 ease-out ${
-                isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
-              }`}
+              className={`hidden sm:block text-xl md:text-5xl lg:text-5xl text-center scale-150 leading-tight bg-gradient-to-r from-[#40ffaa] via-[#4079ff] to-[#40ffaa] bg-clip-text text-transparent transition-all duration-700 ease-out ${isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
+                }`}
               style={{ willChange: "transform, opacity" }}
             >
               <GradientText
@@ -163,23 +161,21 @@ export default function Hero({
             </h2>
 
             <h2
-              className={`block sm:hidden text-3xl md:text-5xl lg:text-5xl text-center scale-150 leading-tight bg-gradient-to-r from-[#40ffaa] via-[#4079ff] to-[#40ffaa] bg-clip-text text-transparent transition-all duration-700 ease-out ${bebas_neue.className} ${
-                isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
-              }`}
+              className={`block sm:hidden text-3xl md:text-5xl lg:text-5xl text-center scale-150 leading-tight  transition-all duration-700 ease-out ${bebas_neue.className} ${isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
+                }`}
               style={{ willChange: "transform, opacity" }}
             >
-              
-                {text2}
-          
+
+              {text2}
+
             </h2>
           </div>
         </div>
 
         {/* Description */}
         <p
-          className={`text-sm sm:text-[18px] font-medium mt-0 sm:mt-2 text-center max-w-6xl text-white/90 leading-relaxed transition-all duration-700 ease-out ${
-            isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
-          }`}
+          className={`text-sm sm:text-[18px] font-medium mt-0 sm:mt-2 text-center max-w-6xl text-white/90 leading-relaxed transition-all duration-700 ease-out ${isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
+            }`}
           style={{ transitionDelay: "150ms", willChange: "transform, opacity" }}
         >
           {description}
@@ -187,14 +183,13 @@ export default function Hero({
 
         {/* Buttons */}
         <div
-          className={`flex flex-row justify-center items-center gap-3 sm:gap-5 mt-4 transition-all duration-700 ease-out ${
-            isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
-          }`}
+          className={`flex flex-row justify-center items-center gap-3 sm:gap-5 mt-4 transition-all duration-700 ease-out ${isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
+            }`}
           style={{ transitionDelay: "300ms", willChange: "transform, opacity" }}
         >
-          <button 
-          onClick={() => setIsModalOpen(true)}
-          className="px-2 sm:px-6 py-3 rounded-full text-md sm:text-lg font-semibold bg-[#B8BBBF] text-[#0F1116] w-fit sm:w-fit hover:bg-white transition-colors cursor-pointer">
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="px-2 sm:px-6 py-3 rounded-full text-md sm:text-lg font-semibold bg-[#B8BBBF] text-[#0F1116] w-fit sm:w-fit hover:bg-white transition-colors cursor-pointer">
             {buttonText}
           </button>
 
@@ -205,9 +200,8 @@ export default function Hero({
 
         {/* Labels */}
         <div
-          className={`w-full sm:hidden flex justify-center items-center gap-1 sm:gap-4 mt-12 transition-all duration-700 ease-out ${
-            isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
-          }`}
+          className={`w-full sm:hidden flex justify-center items-center gap-1 sm:gap-4 mt-12 transition-all duration-700 ease-out ${isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
+            }`}
           style={{ transitionDelay: "450ms", willChange: "transform, opacity" }}
         >
           {[
@@ -221,10 +215,10 @@ export default function Hero({
               text1: "Scalable",
               text2: "Mobile Apps",
             },
-            { 
-              icon: "/home/label-3.svg", 
-              text1: "UI/UX", 
-              text2: "Design & Branding" 
+            {
+              icon: "/home/label-3.svg",
+              text1: "UI/UX",
+              text2: "Design & Branding"
             },
           ].map((item, index) => (
             <div
@@ -243,14 +237,13 @@ export default function Hero({
                 <p className="text-[9px] sm:text-xs text-white/70 leading-tight">{item.text2}</p>
               </div>
             </div>
-            
+
           ))}
         </div>
 
-         <div
-          className={`w-full hidden sm:flex justify-center items-center gap-1 sm:gap-4 mt-12 transition-all duration-700 ease-out ${
-            isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
-          }`}
+        <div
+          className={`w-full hidden sm:flex justify-center items-center gap-1 sm:gap-4 mt-12 transition-all duration-700 ease-out ${isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-12"
+            }`}
           style={{ transitionDelay: "450ms", willChange: "transform, opacity" }}
         >
           {[
@@ -264,10 +257,10 @@ export default function Hero({
               text1: "Scalable",
               text2: "Mobile Apps",
             },
-            { 
-              icon: "/home/label-3.svg", 
-              text1: "UI/UX", 
-              text2: "Design & Branding" 
+            {
+              icon: "/home/label-3.svg",
+              text1: "UI/UX",
+              text2: "Design & Branding"
             },
           ].map((item, index) => (
             <div
@@ -286,13 +279,13 @@ export default function Hero({
                 <p className="text-[9px] sm:text-xs text-white/70 leading-tight">{item.text2}</p>
               </div>
             </div>
-            
+
           ))}
         </div>
       </div>
-      <ContactModal 
-      isOpen={isModalOpen}
-      onClose={() => setIsModalOpen(false)}
+      <ContactModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
       />
     </section>
   );
