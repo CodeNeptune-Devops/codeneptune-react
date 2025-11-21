05b377e5e0241641756771b7c26c018374e28aa6
SANJAYD <sanjayjay564@gmail.com>
2025-11-21 17:54:29 +0530

    updated the robots.txt

--- client/src/app/robots.js
+++ client/src/app/robots.js
@@ -4,12 +4,7 @@ export default function robots() {
       {
         userAgent: "*",
         allow: "/",
-      },
-      {
-        // You can keep this or remove it — it's just to mimic the structure
-        userAgent: "*",
-        disallow: " ",
-      },
+      }
     ],
     sitemap: "https://www.codeneptune.com/sitemap.xml",
     host: "https://www.codeneptune.com",
